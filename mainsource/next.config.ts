11955f772036e42119523c85b9c014ccb5806985
abdulrahman-mm <abdulrahman.mm124@gmail.com>
2026-03-06 10:33:00 +0530

    dynamic generateBuildId added

--- mainsource/next.config.ts
+++ mainsource/next.config.ts
@@ -1,9 +1,14 @@
-// next.config.ts
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  // 1. Force a fresh build ID on every deployment
+  generateBuildId: async () => {
+    return `build-${new Date().getTime()}`;
   },
 };
 
